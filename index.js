76cfed6a0354ba483cff387f84b7a8e43a1d74be
Thibaut Géry <thibaut.gery@gmail.com>
2016-02-05 11:32:04 +0100

    add base app

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const mock = require('./data/test1');
+
+
+app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/secavis/faces/commun/index.jsf', function (req, res) {
+
+  mock.layout = false;
+  res.render('svair', mock);
+});
+
+app.use('/secavis', express.static('public'));
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
