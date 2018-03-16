5185ec89c838241cb4f08ed725ac518585ae03ab
Florian Delezenne <del.florian@gmail.com>
2018-03-16 15:01:50 +0100

    Remove copyright, remove useless page

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/secavis/faces/commun/index.jsf', function (req, res) {
         result.layout = false;
         res.render('svair', Object.assign(defaultData, result));
       } else {
-        res.render('missing', { layout: false });
+        res.redirect('/secavis');
       }
     })
 });
